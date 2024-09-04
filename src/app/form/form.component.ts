02ba4f00e8d00790f0881d2e5d1492fe5fedafb5
Raji G <rajith0417@gmail.com>
2024-09-04 22:08:48 +0530

    feat: form component created

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
+  taskDescription: string = '';
+  @Output() descriptionEvent = new EventEmitter<string>();
+
+  submitTask() {
+    this.descriptionEvent.emit(this.taskDescription);
+    this.taskDescription = "";
+  }
 }
